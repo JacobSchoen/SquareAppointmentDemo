3c50dc291d5b2c39160f4d755bcf45cfc3176743
JacobSchoen <schoen.jacob@gmail.com>
2023-12-02 20:55:22 -0500

    make third party script async

--- src/app/(site)/square/page.jsx
+++ src/app/(site)/square/page.jsx
@@ -3,7 +3,10 @@
 export default async function Success() {
   return (
     <div className="h-10">
-      <script src="https://square.site/appointments/buyer/widget/4ohvg5mp9nb1a8/LNK3KVJFQTGJZ.js"></script>
+      <script
+        src="https://square.site/appointments/buyer/widget/4ohvg5mp9nb1a8/LNK3KVJFQTGJZ.js"
+        async
+      ></script>
     </div>
   );
 }
