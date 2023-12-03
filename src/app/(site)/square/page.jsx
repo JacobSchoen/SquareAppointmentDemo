a66d455af0285722f8ff651d833be95efcb705c7
JacobSchoen <schoen.jacob@gmail.com>
2023-12-02 20:27:06 -0500

    removed some fat, connected square appointment

---
+++ src/app/(site)/square/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default async function Success() {
+  return (
+    <div className="h-10">
+      <script src="https://square.site/appointments/buyer/widget/4ohvg5mp9nb1a8/LNK3KVJFQTGJZ.js"></script>
+    </div>
+  );
+}
